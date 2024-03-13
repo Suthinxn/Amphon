873e839356f4edb989d56ab3b6ad295fecde4679
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-03-13 20:03:35 +0700

    add:file_retieve_info

--- retieve_information.py
+++ retieve_information.py
@@ -3,10 +3,10 @@ from pprint import pformat
 
 station_id = "44t"
 # param = "PM25,PM10,O3,CO,NO2,SO2,WS,TEMP,RH,WD"
-param = "PM25,TEMP,WS,RH,WD,RAIN"
+param = "PM25,TEMP,WS,RH,WD"
 data_type = "hr"
-start_date = "2024-03-01"
-end_date = "2024-03-11"
+start_date = "2024-01-01"
+end_date = "2024-03-12"
 start_time = "00"
 end_time = "23"
 url = f"http://air4thai.com/forweb/getHistoryData.php?stationID={station_id}&param={param}&type={data_type}&sdate={start_date}&edate={end_date}&stime={start_time}&etime={end_time}"
